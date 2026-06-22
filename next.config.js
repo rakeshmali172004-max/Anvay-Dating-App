17a92d87c626f848fe34f1d2aa03a22267b086d4
rakeshmali172004-max <rakeshmali172004@gmail.com>
2026-06-22 15:09:36 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  }
 };
 
-module.exports = nextConfig;
+export default nextConfig;
 
 
