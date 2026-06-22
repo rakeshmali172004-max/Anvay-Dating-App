04bca53a288fae9e8db0ddd8221c82431e8cb2ee
rakeshmali172004-max <rakeshmali172004@gmail.com>
2026-06-22 16:14:13 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
+  reactStrictMode: true,
+  env: {
+    BUILD_TRIGGER: "force_rebuild_v2"
   }
 };
 
 export default nextConfig;
-
-
